0bab38ad1d223051479687612c7a428b4b816798
Benjamin <1234hteimaj4321@gmail.com>
2020-12-10 17:56:38 -0700

    added gh user's img

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -5,6 +5,7 @@ export const Test = () => {
   const [users, setUsers] = useState([]);
   useEffect(async () => {
     const github = await axios.get("https://api.github.com/users");
+
     setUsers(github.data);
   }, []);
 
@@ -12,7 +13,8 @@ export const Test = () => {
   return (
     <>
       {users.map((user) => (
-        <div>
+        <div key={user.node_id}>
+          <img src={user.avatar_url} />
           <a href={user.url}>{user.login}'s page</a>
         </div>
       ))}
